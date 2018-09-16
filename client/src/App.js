e8f72858aa291deea8f43d7090cc5bc30edd5679
DrSleepy <m-subahi@hotmail.com>
2018-09-16 17:15:58 +0100

    Finished header of RoomView

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
-import HomeView from './components/HomeView/HomeView';
+// import HomeView from './components/HomeView/HomeView';
+import RoomView from './components/RoomView/RoomView';
 
 import './styles/reset.less';
 import './styles/base.less';
@@ -9,7 +10,8 @@ class App extends Component {
   render() {
     return (
       <div>
-        <HomeView />
+        {/* <HomeView /> */}
+        <RoomView />
       </div>
     );
   }
--- client/src/components/RoomView/RoomView.jsx
+++ client/src/components/RoomView/RoomView.jsx
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import RoomInfo from '../RoomInfo/RoomInfo';
 import css from './roomView.less';
 
 const RoomView = props => (
-  <div className={css['room-view']}>
-    <i className={css['room-view__back']} />
-    <div className={css['room-view__room-info']}>
-      <RoomInfo />
-    </div>
-    <i className={css['room-view__leave']} />
-    <i className={css['room-view__settings']} />
-  </div>
+  <Fragment>
+    <header>
+      <div className={css['room']}>
+        <i className={css['room__back']} />
+        <div className={css['room__room-info']}>
+          <RoomInfo />
+        </div>
+        <i className={css['room__leave']} />
+        <i className={css['room__settings']} />
+      </div>
+      <div className={css['actions']}>
+        <button className={css['actions__submit']}> Create Question </button>
+        <button>
+          <span className={[css['actions__item'], css['actions__item--search']].join(' ')}> Search </span>
+        </button>
+        <button>
+          <span className={[css['actions__item'], css['actions__item--view']].join(' ')}> View </span>
+        </button>
+        <button>
+          <span className={[css['actions__item'], css['actions__item--sort']].join(' ')}> Sort </span>
+        </button>
+      </div>
+    </header>
+    <main />
+  </Fragment>
 );
 
 export default RoomView;
