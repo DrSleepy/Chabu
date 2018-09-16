ab66bc73d54c178ae970c0b79688a16a3de92a40
DrSleepy <m-subahi@hotmail.com>
2018-09-16 16:06:35 +0100

    updaded classNames to be consistent

--- client/src/components/HomeView/HomeView.jsx
+++ client/src/components/HomeView/HomeView.jsx
@@ -2,24 +2,24 @@ import React, { Fragment } from 'react';
 
 import RoomItem from '../RoomItem/RoomItem';
 import RoomView from '../RoomView/RoomView';
+import QuestionItem from '../QuestionItem/QuestionItem';
 import css from './homeView.less';
 
 const Home = props => (
   <Fragment>
-    <header>
-      <div className={css.head}>
-        <button className={css.head__join}> Join room </button>
-        <button className={css.head__create}> Create room </button>
-        <i className={css.head__settings}> </i>
-      </div>
-      <nav>
-        <ul className={css.navigation}>
-          <li className={css.navigation__item}> Joined Rooms </li>
-          <li className={css.navigation__item}> My Questions </li>
-          <li className={css.navigation__item}> Created Rooms </li>
-        </ul>
-      </nav>
-    </header>
+    <div className={css['head']}>
+      <button className={css['head__join']}> Join room </button>
+      <button className={css['head__create']}> Create room </button>
+      <i className={css['head__settings']}> </i>
+    </div>
+    <nav>
+      <ul className={css['navigation']}>
+        <li className={css['navigation__item']}> Joined Rooms </li>
+        <li className={css['navigation__item']}> My Questions </li>
+        <li className={css['navigation__item']}> Created Rooms </li>
+      </ul>
+    </nav>
+    <QuestionItem />
     <RoomItem />
     <RoomView />
   </Fragment>
--- client/src/components/QuestionItem/QuestionItem.jsx
+++ client/src/components/QuestionItem/QuestionItem.jsx
@@ -3,15 +3,15 @@ import React from 'react';
 import css from './questionItem.less';
 
 const QuestionItem = props => (
-  <div className={css.question}>
-    <i className={css.question__thumb} />
-    <h2 className={css.question__title}>
+  <div className={css['question']}>
+    <i className={css['question__thumb']} />
+    <h2 className={css['question__title']}>
       What exactly are pipelines in the context of Computer Science? Secondly, how does this fit into the exam?
     </h2>
-    <p className={css.question__likes}> 1 likes </p>
-    <p className={css.question__comments}> 3 comments </p>
-    <p className={css.question__time}> 4 hours ago </p>
-    <i className={css.question__delete} />
+    <p className={css['question__likes']}> 1 likes </p>
+    <p className={css['question__comments']}> 3 comments </p>
+    <p className={css['question__time']}> 4 hours ago </p>
+    <i className={css['question__delete']} />
   </div>
 );
 
